8d446d8b188c589ad475db2439a749123023052d
dmlv <davismedins@gmail.com>
2022-12-06 16:31:30 +0000

    update db insert checker

--- test_db.py
+++ test_db.py
@@ -30,6 +30,8 @@ def get_cursor():
                 connection.commit()
         return connection.cursor()
 
+cursor = get_cursor()
+cursor = connection.cursor(buffered=True)
 #Check if it is possible to connect to database
 print("Checking if it is possible to connect to database")
 assert connection.is_connected() == True
@@ -38,28 +40,30 @@ print("----------")
 
 #Check if it is possible to write to database
 print("Checking if it is possible to write to database")
-cursor = get_cursor()
+cursor.execute("SELECT * FROM ast_daily")
+count = cursor.rowcount
 try:
-	cursor = connection.cursor()
 	result  = cursor.execute( "INSERT INTO `ast_daily` (`create_date`, `hazardous`, `name`, `url`, `diam_min`, `diam_max`, `ts`, `dt_utc`, `dt_local`, `speed`, `distance`, `ast_id`) VALUES ('testDate', '0', 'testName', 'testURL', '10.5', '99.9', '1234567890', 'testDateUTC', 'testDateLocal', '1234567890', '1234567890', '12345')")
 	connection.commit()
 except Error as e :
 	print('Problem inserting asteroid values into DB: ' + str(e))
 	pass
-
-assert cursor.fetchwarnings() == None
+cursor.execute("SELECT * FROM ast_daily")
+newCount = cursor.rowcount
+print(count, " rows before insertion")
+print(newCount, " rows after insertion")
+assert newCount > count
 print("SUCCESS!!")
 print("-----------")
 
 #Check if it possible to write to databes with incorrect data types
 print("Checking if it is possible to insert invalid data types into database")
-cursor = get_cursor()
-try:
-	cursor = connection.cursor()
-	result  = cursor.execute( "INSERT INTO `ast_daily` (`create_date`, `hazardous`, `name`, `url`, `diam_min`, `diam_max`, `ts`, `dt_utc`, `dt_local`, `speed`, `distance`, `ast_id`) VALUES ('10.5', 'yes', 'testName', 'testURL', 'yes', 'yes', '12.5', 'testDateUTC', 'testDateLocal', '123456789012', '1234567890.56', 'value')")
-	connection.commit()
-except Error as e :
-	print('Problem inserting asteroid values into DB: ' + str(e))
-	pass
+#try:
+#	cursor = connection.cursor()
+#	result  = cursor.execute( "INSERT INTO `ast_daily` (`create_date`, `hazardous`, `name`, `url`, `diam_min`, `diam_max`, `ts`, `dt_utc`, `dt_local`, `speed`, `distance`, `ast_id`) VALUES ('10.5', 'yes', 'testName', 'testURL', 'yes', 'yes', '12.5', 'testDateUTC', 'testDateLocal', '123456789012', '1234567890.56', 'value')")
+#	connection.commit()
+#except Error as e :
+#	print('Problem inserting asteroid values into DB: ' + str(e))
+#	pass
 
-assert cursor.fetchwarnings() != None
+#print(cursor.fetchwarnings())
